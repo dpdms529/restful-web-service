47c61e6695d652875ef67a12f88b4e65e60d4b9f
YE-EUN <musiclove529@naver.com>
2023-06-30 00:40:25 +0900

    섹션 3. RESTful Service 기능 확장

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +14,16 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value = {"password"})
+@JsonFilter("UserInfo")
 public class User {
     private Integer id;
+    @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
     private String name;
+    @Past
     private Date joinDate;
+    //@JsonIgnore
+    private String password;
+    //@JsonIgnore
+    private String ssn;
 }
